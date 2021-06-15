bc1ee1b4aba43fb3bf17e80be5b084c0970521c1
pudu87 <sam.cappelle87@gmail.com>
2021-06-15 17:48:14 -0400

    add content of about

--- src/about.js
+++ src/about.js
@@ -1,7 +1,37 @@
+import mannekenpis from './img/manneken-pis.jpg'
+import santiago from './img/santiago.jpeg'
+
 const content = document.querySelector('#content');
 
+const loadHeader = () => {
+  const header = document.createElement('header');
+    const p1 = document.createElement('p');
+    p1.textContent = "I'm a Belgian";
+    const img1 = document.createElement('img');
+    img1.src = mannekenpis;
+    const p2 = document.createElement('p');
+    p2.textContent = "Living in Chile";
+    const img2 = document.createElement('img');
+    img2.src = santiago;
+    const p3 = document.createElement('p');
+    p3.textContent = 'Looking for a job or internship';
+  [p1, img1, p2, img2, p3].forEach(element => {
+    header.append(element);
+  });
+  return header;
+}
+
+const loadPersonalData = () => {
+  const section = document.createElement('section');
+    const p1 = document.createElement('p');
+    p1.textContent = 'Click over here to find my CV';
+  section.append(p1);
+  return section;
+}
+
 const loadAbout = () => {
-  content.textContent = 'About';
+  content.append(loadHeader());
+  content.append(loadPersonalData());
 }
 
 export default loadAbout
