ed5f9061dff734f0bce5ddfa98bf7fa57a65e9b4
pudu87 <sam.cappelle87@gmail.com>
2021-06-15 12:55:06 -0400

    move assets to src file

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,21 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource'
-      }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
